f56047a5b84e51159e2ccb06b5f02642b8102970
Andy Zhang <az@MacBook-Pro-738.local>
2024-10-13 23:59:18 -0700

    minor fix

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -37,7 +37,7 @@ export default function Projects() {
             <div className="hidden lg:flex flex-col justify-start mt-4 lg:mt-0 lg:ml-6">
               <p className="text-lg font-semibold text-gray-900 dark:text-white">Tech Stack:</p>
               <div className="flex flex-col gap-3 mt-2">
-                {["Python", "Qauntum Computing"].map((tech, index) => (
+                {["Python", "Quantum Computing"].map((tech, index) => (
                   <span 
                     key={index} 
                     className="text-md font-medium text-gray-900 dark:text-gray-100 transform transition duration-300 hover:-translate-y-1 hover:text-indigo-500 dark:hover:text-indigo-400"
